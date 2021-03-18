1d64393f0aa6b6e72352d778976db76a68f59fb2
Thiago Almeida <almeida.thiagoalvesde@gmail.com>
2021-03-17 21:23:20 -0300

    Entrega da atividade 5

---
+++ Atividade5/scriptA.js
@@ -0,0 +1,7 @@
+var nota1 = parseFloat(prompt('Digite a nota 1'));
+var nota2 = parseFloat(prompt('Digite a nota 2'));
+var nota3 = parseFloat(prompt('Digite a nota 3'));
+
+var media = (nota1 + nota2 + nota3)/3;
+
+alert(`A media é: ${media}`);
---
+++ Atividade5/scriptB.js
@@ -0,0 +1,12 @@
+var num1 = parseFloat(prompt('Digite o primeiro número'));
+var num2 = parseFloat(prompt('Digite o segundo número'));
+
+alert(`A soma dos dois números é: ${num1 + num2}`);
+
+alert(`A subtração do primeiro pelo segundo número é: ${num1 - num2}`);
+
+alert(`O produto dos dois é: ${num1 * num2}`);
+
+alert(`A divisão do primeiro pelo segundo é: ${num1 / num2}`);
+
+alert(`O resto da divisão do primeiro pelo segundo é: ${num1 % num2}`);
